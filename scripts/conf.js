03fd1bc0123743d35a2b33c420690fb45ecb1d1f
Matt Campbell <matt@soupmatt.com>
2015-06-19 17:21:28 -0500

    add config file

---
+++ scripts/conf.js
@@ -0,0 +1,11 @@
+/* Copy conf.js-dist to conf.js
+   and set it up for your environment.
+
+   DO NOT USE ANY FILESYSTEM PATH */
+
+define({
+
+  /* URL to access your Tiny Tiny RSS installation */
+  apiPath: "/"
+
+});
